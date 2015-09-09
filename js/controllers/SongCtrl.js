da7833fb6d4edd2c7d34742729b25d22499b13d8
Sarah Story <sarah.f.lavendol@vanderbilt.edu>
2015-09-09 12:22:46 -0500

    troubleshooting

--- js/controllers/SongCtrl.js
+++ js/controllers/SongCtrl.js
@@ -16,7 +16,7 @@ app.controller("SongCtrl", ["Auth", "$scope", "$firebaseArray", "$firebaseAuth",
   };
 
   $scope.rightMenu = function() {
-    $scope.sideMenuController.toggleRight();
+    $ionicSideMenuDelegate.toggleRight();
   };
-
+  
 }]);
