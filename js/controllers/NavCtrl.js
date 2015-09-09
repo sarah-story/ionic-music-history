b236085e357afa64695f39d7fff5e4ff96bd2bfe
Sarah Story <sarah.f.lavendol@vanderbilt.edu>
2015-09-09 11:54:26 -0500

    added a comma

--- js/controllers/NavCtrl.js
+++ js/controllers/NavCtrl.js
@@ -1,4 +1,4 @@
-app.controller("NavCtrl", ["$scope", "$ionicSideMenuDelegate", function($scope$ionicSideMenuDelegate) {
+app.controller("NavCtrl", ["$scope", "$ionicSideMenuDelegate", function($scope, $ionicSideMenuDelegate) {
   $scope.rightMenu = function() {
     $ionicSideMenuDelegate.toggleRight();
   };
